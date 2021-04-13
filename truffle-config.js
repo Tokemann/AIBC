07a67bf71c2153e535632a9d1aa85791251d76b8
rahul-mytokenized-capital <rahul@mytokenized.capital>
2021-04-13 18:15:34 +0530

    Adding changes

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,7 @@
 //
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   /**
@@ -40,7 +40,7 @@ module.exports = {
 	  
 	  development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
 	
@@ -48,7 +48,7 @@ module.exports = {
       provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
       network_id: 80001,
       confirmations: 2,
-      timeoutBlocks: 200,
+      timeoutBlocks: 2000,
       skipDryRun: true
     },
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -97,7 +97,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
