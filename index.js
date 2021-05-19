0284fc2089f2a374957794fa965f968bca121f38
rahul-mytokenized-capital <rahul@mytokenized.capital>
2021-05-19 18:30:19 +0530

    Adding fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const GenesisContract = require("./build/contracts/GenesisContract.json");
-const MyTcErc20 = require("./build/contracts/MyTcErc20.json");
+const MYTC = require("./build/contracts/MYTC.json");
 const CustomERC20 = require("./build/abi/CustomERC20.abi");
 const CryptoFace = require("./build/contracts/CryptoFace.json");
 const CustomERC20Contract = require("./build/contracts/CustomERC20.json");
 
 module.exports.GenesisContract = GenesisContract;
-module.exports.MyTcErc20 = MyTcErc20;
+module.exports.MYTC = MYTC;
 module.exports.CustomERC20=CustomERC20;
 module.exports.CryptoFace=CryptoFace;
 module.exports.CustomERC20Contract=CustomERC20Contract;
